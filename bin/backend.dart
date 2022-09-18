ea5b9f62ada7034bc2ef6b34f565ff86fc8a15f1
Jessica <jessicaviana@ext.cvccorp.com.br>
2022-09-17 22:21:56 -0300

    server

--- bin/backend.dart
+++ bin/backend.dart
@@ -1,5 +1,35 @@
-import 'package:backend/backend.dart' as backend;
+import 'dart:async';
 
-void main(List<String> arguments) {
-  print('Hello world: ${backend.calculate()}!');
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart' as shelf;
+
+void main(List<String> arguments) async {
+  final pipeline = Pipeline().addMiddleware(log());
+
+  final server = await shelf.serve(
+    pipeline.addHandler(handler),
+    '0.0.0.0',
+    8080,
+  );
+
+  print('Online - ${server.address.host}:${server.port}');
+}
+
+FutureOr<Response> handler(Request request) {
+  final response = Response(200, body: "html");
+  return response;
+}
+
+Middleware log() {
+  return (handler) {
+    return (request) async {
+      print('solicitado: ${request.url}');
+      //antes de executar
+      var response = await handler(request);
+      //depois de executar
+      print('resposta: ${response.statusCode}');
+
+      return response;
+    };
+  };
 }
