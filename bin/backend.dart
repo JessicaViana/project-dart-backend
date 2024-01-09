65ff134844e193407ad4aa8004e5ea3b37ddfc74
Jessica <jessicaviana@ext.cvccorp.com.br>
2024-01-09 20:48:48 -0300

    conexão do projeto com db

--- bin/backend.dart
+++ bin/backend.dart
@@ -1,3 +1,4 @@
+import 'package:mysql1/mysql1.dart';
 import 'package:shelf/shelf.dart';
 
 import 'apis/blog_api.dart';
@@ -9,6 +10,19 @@ import 'infra/utils/custom_env_helper.dart';
 
 void main(List<String> arguments) async {
   final di = Injection.initialize();
+
+  var sqlConfig = ConnectionSettings(
+    host: await CustomEnvHelper.get<String>(key: 'SERVER_ADDRESS'),
+    port: await CustomEnvHelper.get<int>(key: 'DB_PORT'),
+    db: await CustomEnvHelper.get<String>(key: 'DB_SCHEMA'),
+    user: await CustomEnvHelper.get<String>(key: 'DB_USER'),
+    password: await CustomEnvHelper.get<String>(key: 'DB_PASS'),
+  );
+  var sqlConnection = await MySqlConnection.connect(sqlConfig);
+
+  var sqlQuery = await sqlConnection.query('SELECT * from usuarios');
+
+  print(sqlQuery);
 
   final handlerCascade = Cascade()
       .add(
