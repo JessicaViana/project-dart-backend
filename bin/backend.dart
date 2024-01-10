2d0f071e002acf4c1b4cffc9b084208e88e0d225
Jessica <jessicaviana@ext.cvccorp.com.br>
2024-01-09 22:30:50 -0300

    abstração e implementação

--- bin/backend.dart
+++ bin/backend.dart
@@ -1,9 +1,9 @@
-import 'package:mysql1/mysql1.dart';
 import 'package:shelf/shelf.dart';
 
 import 'apis/blog_api.dart';
 import 'apis/login_api.dart';
 import 'infra/custom_server.dart';
+import 'infra/database/db_connection_impl.dart';
 import 'infra/dependency_injector/injection.dart';
 import 'infra/middleware_interception.dart';
 import 'infra/utils/custom_env_helper.dart';
@@ -11,16 +11,9 @@ import 'infra/utils/custom_env_helper.dart';
 void main(List<String> arguments) async {
   final di = Injection.initialize();
 
-  var sqlConfig = ConnectionSettings(
-    host: await CustomEnvHelper.get<String>(key: 'SERVER_ADDRESS'),
-    port: await CustomEnvHelper.get<int>(key: 'DB_PORT'),
-    db: await CustomEnvHelper.get<String>(key: 'DB_SCHEMA'),
-    user: await CustomEnvHelper.get<String>(key: 'DB_USER'),
-    password: await CustomEnvHelper.get<String>(key: 'DB_PASS'),
-  );
-  var sqlConnection = await MySqlConnection.connect(sqlConfig);
+  var conexao = await DbConnectionImpl().connection;
 
-  var sqlQuery = await sqlConnection.query('SELECT * from usuarios');
+  var sqlQuery = await conexao.query('SELECT * from usuarios');
 
   print(sqlQuery);
 
